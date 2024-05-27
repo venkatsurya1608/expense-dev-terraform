7631a872d7e92515e2884968d944a064005e7835
venkatsurya <venkatsurya1608@gmail.com>
2024-05-27 22:08:23 +0530

    expense-vpc

---
+++ 01-vpc/varables.tf
@@ -0,0 +1,54 @@
+variable "project_name" {
+    default = "expense"
+  
+}
+variable "environment" {
+    default = "dev"
+  
+}
+# variable "sg_group" {
+#     default = "db"
+  
+# }
+
+variable "common_tags" {
+    default = {
+        project = "expense"
+        environment = "dev"
+        terraform = "true"
+    }
+  
+}
+
+variable "public_subnet_cidrs" {
+    default = ["10.0.1.0/24","10.0.2.0/24" ]
+  
+}
+
+variable "private_subnet_cidrs" {
+    default = ["10.0.15.0/24","10.0.17.0/24"]
+  
+}
+
+variable "database_subnet_cidrs" {
+    default = ["10.0.101.0/24","10.0.102.0/24"]
+  
+}
+
+# peering connection ##
+variable "is_peering_required" {
+    type = bool
+    default = false
+  
+}
+
+# variable "acceptor_vpc_id" {
+#     type = string
+#     default = ""     # ""  this means default value 
+  
+# }
+
+# variable "vpc_peering_tags" {
+#     default = {}
+  
+# }
---
+++ 02-sg/variables.tf
@@ -0,0 +1,17 @@
+variable "project_name" {
+    default = "expense"
+  
+}
+
+variable "environment" {
+    default = "dev"
+  
+}
+variable "common_tags" {
+    default = {
+        project = "expense"
+        environment = "dev"
+        terraform = "true"
+    }
+  
+}
